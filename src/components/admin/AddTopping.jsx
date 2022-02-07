5f68e4db3f6319cf656989707e860ec8fb2ab9a0
karunialeo <karunialeo@gmail.com>
2022-02-07 22:43:59 +0700

    frontend complete, need refactoring

--- src/components/admin/AddTopping.jsx
+++ src/components/admin/AddTopping.jsx
@@ -23,12 +23,23 @@ export default function AddTopping(props) {
               placeholder="Price"
               className="w-full p-3 outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100"
             />
-            <input
-              type="text"
-              name="addToppingPhoto"
-              placeholder="Photo"
-              className="w-full p-3 outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100"
-            />
+            <label
+              htmlFor="addToppingPhoto"
+              className="block text-left w-full p-3 outline outline-2 cursor-pointer outline-red-500 focus:outline-red-700 rounded-md bg-pink-100"
+            >
+              <div className="flex justify-between">
+                <p className="text-sm  opacity-100 font-normal text-gray-400">
+                  Photo Topping
+                </p>
+                <input
+                  id="addToppingPhoto"
+                  name="addToppingPhoto"
+                  type="file"
+                  className="sr-only"
+                />
+                <img className="h-5 mr-2" src="/img/upload.png" alt="" />
+              </div>
+            </label>
             <button className="w-full lg:w-9/12 py-2 rounded-md text-white text-center bg-brand-red">
               Add Topping
             </button>

60e40f60ed724a56f6fe2cc786a2956d3f05977b
karunialeo <karunialeo@gmail.com>
2022-03-26 00:36:39 +0700

    ready for netlify

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL:
+    process.env.REACT_APP_SERVER_URL ||
+    "https://karunialeo-waysbucks-api.herokuapp.com/",
 });
 
 export const setAuthToken = (token) => {
